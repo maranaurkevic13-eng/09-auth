2739429e9b88412c1f6334db1babddc4f9b74313
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-06-16 18:55:47 +0300

    save proxy

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -1,30 +1,29 @@
 "use client";
 
-import { useState } from "react";  
+import { useState } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api/clientApi";
-import { useDebounce } from "@/lib/hooks/useDebounce";
+import { useDebouncedCallback } from "use-debounce";
 import SearchBox from "@/components/SearchBox/SearchBox";
 import NoteList from "@/components/NoteList/NoteList";
 import Pagination from "@/components/Pagination/Pagination";
-import Link from "next/link";    
+import Link from "next/link";
 
 export default function NotesClient({ tag }: { tag?: string }) {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
 
-  const debouncedSearch = useDebounce(search, 500);
+
+  const debouncedSearch = useDebouncedCallback((value: string) => {
+    setSearch(value);
+    setPage(1); 
+  }, 500);
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ["notes", page, debouncedSearch, tag],
-    queryFn: () => fetchNotes(page, 10, debouncedSearch, tag),
+    queryKey: ["notes", page, search, tag],
+    queryFn: () => fetchNotes(page, 10, search, tag),
     placeholderData: keepPreviousData,
-  });
-
-  const handleSearch = (value: string) => {
-    setSearch(value);
-    setPage(1);
-  };
+  });    
 
   if (isLoading) return <p>Loading notes...</p>;
   if (isError) return <p>Error loading notes</p>;
@@ -35,11 +34,12 @@ export default function NotesClient({ tag }: { tag?: string }) {
   return (
     <main>
       <div className="notes-header">
-        <SearchBox value={search} onSearch={handleSearch} />
+        <SearchBox value={search} onSearch={debouncedSearch} />
         <Link href="/notes/action/create">Create Note</Link>
       </div>
 
       <NoteList notes={notes} />
+
       {notes.length > 0 && (
         <Pagination
           currentPage={page}

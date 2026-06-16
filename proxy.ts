c98c0caaa5d508dc62ab38863dd4ddaed16500e1
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-06-16 12:43:43 +0300

    add export proxy

--- proxy.ts
+++ proxy.ts
@@ -4,16 +4,16 @@ import type { NextRequest } from "next/server";
 const privateRoutes = ["/profile", "/notes"];
 const publicRoutes = ["/sign-in", "/sign-up"];
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const token = req.cookies.get("token"); 
   const { pathname } = req.nextUrl;
 
-  // Якщо користувач неавторизований і відкриває приватний маршрут 
+  // Якщо неавторизований
   if (!token && privateRoutes.some((route) => pathname.startsWith(route))) {
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
-  // Якщо користувач авторизований і відкриває публічний маршрут 
+  // Якщо авторизований 
   if (token && publicRoutes.some((route) => pathname.startsWith(route))) {
     return NextResponse.redirect(new URL("/profile", req.url));
   }
@@ -21,6 +21,3 @@ export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/profile/:path*", "/notes/:path*", "/sign-in", "/sign-up"],
-};
